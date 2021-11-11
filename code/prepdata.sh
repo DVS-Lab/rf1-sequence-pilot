fd5016aaac370d72b6dcc488948011178b540466
DVSneuro <david.v.smith@temple.edu>
2021-11-11 17:46:40 -0500

    initial changes

--- code/prepdata.sh
+++ code/prepdata.sh
@@ -10,7 +10,7 @@
 # 1) containers live under /data/tools on local computer. should these relative paths and shared? YODA principles would suggest so.
 # 2) other projects should use Jeff's python script for fixing the IntendedFor
 # 3) aside from containers, only absolute path in whole workflow (transparent to folks who aren't allowed to access to raw data)
-sourcedata=/data/sourcedata/istart
+sourcedata=/data/sourcedata/rf1-sequence-pilot
 
 
 sub=$1
@@ -32,11 +32,13 @@ rm -rf $dsroot/bids/sub-${sub}
 
 
 # PART 1: running heudiconv and fixing fieldmaps
+#/data/sourcedata/rf1-sequence-pilot/MB_ME_test_sub-0001/dicoms/9-CMRR_MB3_IP2_ME1_TR1850/resources/DICOM/files
+
 singularity run --cleanenv \
 -B $dsroot:/out \
 -B $sourcedata:/sourcedata \
 /data/tools/heudiconv-0.9.0.simg \
--d /sourcedata/dicoms/Smith-ISTART-{subject}/scans/*/resources/DICOM/files/*.dcm \
+-d /sourcedata/MB_ME_test_sub-{subject}/dicoms/*/resources/DICOM/files/*.dcm \
 -s $sub \
 -f /out/code/heuristics.py \
 -c dcm2niix -b --minmeta -o /out/bids --overwrite
@@ -46,44 +48,44 @@ chmod -R uga+rw $dsroot/bids/sub-$sub
 python $codedir/addIntendedFor.py
 
 
-
-# PART 2: Defacing anatomicals and date shifting to ensure compatibility with data sharing.
-
-# note that you may need to install pydeface via pip or conda
-bidsroot=$dsroot/bids
-pydeface ${bidsroot}/sub-${sub}/anat/sub-${sub}_T1w.nii.gz
-mv -f ${bidsroot}/sub-${sub}/anat/sub-${sub}_T1w_defaced.nii.gz ${bidsroot}/sub-${sub}/anat/sub-${sub}_T1w.nii.gz
-
-# shift dates on scans to reduce likelihood of re-identification
-python $codedir/shiftdates.py $dsroot/bids/sub-${sub}/sub-${sub}_scans.tsv
-
-
-
-# PART 3: Run MRIQC on subject
-
-# make derivatives folder if it doesn't exist.
-# let's keep this out of bids for now
-if [ ! -d $dsroot/derivatives/mriqc ]; then
-	mkdir -p $dsroot/derivatives/mriqc
-fi
-
-
-# make scratch
-scratch=/data/scratch/`whoami`
-if [ ! -d $scratch ]; then
-	mkdir -p $scratch
-fi
-
-# no space left on device error for v0.15.2 and higher
-# https://neurostars.org/t/mriqc-no-space-left-on-device-error/16187/1
-# https://github.com/poldracklab/mriqc/issues/850
-TEMPLATEFLOW_DIR=/data/tools/templateflow
-export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
-singularity run --cleanenv \
--B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
--B $dsroot/bids:/data \
--B $dsroot/derivatives/mriqc:/out \
--B $scratch:/scratch \
-/data/tools/mriqc-0.16.1.simg \
-/data /out \
-participant --participant_label $sub -w /scratch
+#
+## PART 2: Defacing anatomicals and date shifting to ensure compatibility with data sharing.
+#
+## note that you may need to install pydeface via pip or conda
+#bidsroot=$dsroot/bids
+#pydeface ${bidsroot}/sub-${sub}/anat/sub-${sub}_T1w.nii.gz
+#mv -f ${bidsroot}/sub-${sub}/anat/sub-${sub}_T1w_defaced.nii.gz ${bidsroot}/sub-${sub}/anat/sub-${sub}_T1w.nii.gz
+#
+## shift dates on scans to reduce likelihood of re-identification
+#python $codedir/shiftdates.py $dsroot/bids/sub-${sub}/sub-${sub}_scans.tsv
+#
+#
+#
+## PART 3: Run MRIQC on subject
+#
+## make derivatives folder if it doesn't exist.
+## let's keep this out of bids for now
+#if [ ! -d $dsroot/derivatives/mriqc ]; then
+#	mkdir -p $dsroot/derivatives/mriqc
+#fi
+#
+#
+## make scratch
+#scratch=/data/scratch/`whoami`
+#if [ ! -d $scratch ]; then
+#	mkdir -p $scratch
+#fi
+#
+## no space left on device error for v0.15.2 and higher
+## https://neurostars.org/t/mriqc-no-space-left-on-device-error/16187/1
+## https://github.com/poldracklab/mriqc/issues/850
+#TEMPLATEFLOW_DIR=/data/tools/templateflow
+#export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
+#singularity run --cleanenv \
+#-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+#-B $dsroot/bids:/data \
+#-B $dsroot/derivatives/mriqc:/out \
+#-B $scratch:/scratch \
+#/data/tools/mriqc-0.16.1.simg \
+#/data /out \
+#participant --participant_label $sub -w /scratch
