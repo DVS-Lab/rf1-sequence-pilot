715e62e27ce9697a20aae9fbfb91dbcd060b7923
DVSneuro <david.v.smith@temple.edu>
2021-11-12 21:10:15 -0500

    initial changes for sequence test

--- code/tsnr.sh
+++ code/tsnr.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# remove phase (?) image. not sure why these are there
-rm -rf *_ph.nii
+# ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
 
-for i in `ls -1 *.nii`; do 
+sub=$1
 
-	# remove small files
-	nvols=`fslnvols $i`
-	if [ $nvols -lt 20 ]; then
-		rm -rf $i
-	fi
-	
+datadir=$maindir/derivatives/fmriprep/sub-${sub}/func
+cd $datadir
+
+for i in `ls -1 *_bold.nii.gz`; do
+
 	fslmaths $i -Tmean tmp_mean
 	fslmaths $i -Tstd tmp_std
 	fslmaths tmp_mean -div tmp_std tmp_tsnr
@@ -18,6 +18,5 @@ for i in `ls -1 *.nii`; do
 	max=`fslstats thr_tmp_tsnr -R | awk '{ print $2 }'`
 	mean=`fslstats thr_tmp_tsnr -M`
 	echo -e "$i\t mean tsnr: $mean\t max tsnr: $max"
-	
-done	
 
+done
