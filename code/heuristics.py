5a4ad262e7bc992e4d2a0a8f9cbffe326c67aa82
DVSneuro <david.v.smith@temple.edu>
2021-11-21 13:52:52 -0500

    me4 and task

--- code/heuristics.py
+++ code/heuristics.py
@@ -10,59 +10,40 @@ def infotodict(seqinfo):
     # mag = create_key('sub-{subject}/fmap/sub-{subject}_magnitude')
     # phase = create_key('sub-{subject}/fmap/sub-{subject}_phasediff')
 
-    # missing sbref for the following?
-    # 7-CMRR_MB1_IP2_ME1_TR1850
-    # 12-CMRR_MB1_IP2_ME3_TR1850
 
     #me1
-    rest_mb1me1 =       create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb1me1_bold')
-    #rest_mb1me1_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb1me1_sbref')
-    rest_mb3me1 =       create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me1_bold')
-    rest_mb3me1_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me1_sbref')
-    rest_mb6me1 =       create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb6me1_bold')
-    rest_mb6me1_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb6me1_sbref')
+    mb1me1 =       create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb1me1_bold')
+    mb3me1 =       create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb3me1_bold')
+    mb3me1_sbref = create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb3me1_sbref')
+    mb6me1 =       create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb6me1_bold')
+    mb6me1_sbref = create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb6me1_sbref')
 
-    #me3
-    rest_mb1me3 =            create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb1me3_bold')
-    #rest_mb1me3_sbref =      create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb1me3_sbref')
-    rest_mb3me3 =            create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me3_bold')
-    rest_mb3me3_sbref =      create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me3_sbref')
-    rest_mb6me3 =            create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb6me3_bold')
-    rest_mb6me3_sbref =      create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb6me3_sbref')
-    rest_mb3me3noInt =       create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me3noInt_bold')
-    rest_mb3me3noInt_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me3noInt_sbref')
+    #me4
+    mb1me4 =            create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb1me4_bold')
+    mb3me4 =            create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb3me4_bold')
+    mb3me4_sbref =      create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb3me4_sbref')
+    mb6me4 =            create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb6me4_bold')
+    mb6me4_sbref =      create_key('sub-{subject}/func/sub-{subject}_task-sharedreward_acq-mb6me4_sbref')
 
-    # note: didn't get sbref for CMRR_MB1_IP2_ME3_TR1850
 
-    #other
-    rest_mb3me4 =       create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me4_bold')
-    rest_mb3me4_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me4_sbref')
-    rest_mb3me3 =       create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me3_bold')
-    rest_mb3me3_sbref = create_key('sub-{subject}/func/sub-{subject}_task-rest_acq-mb3me3_sbref')
 
         # mag: [],
         # phase: [],
 
     info = {t1w: [],
 
-            rest_mb1me1: [],
-            #rest_mb1me1_sbref: [],
-            rest_mb3me1: [],
-            rest_mb3me1_sbref: [],
-            rest_mb6me1: [],
-            rest_mb6me1_sbref: [],
+            mb1me1: [],
+            mb3me1: [],
+            mb3me1_sbref: [],
+            mb6me1: [],
+            mb6me1_sbref: [],
 
-            rest_mb1me3: [],
-            #rest_mb1me3_sbref: [],
-            rest_mb3me3: [],
-            rest_mb3me3_sbref: [],
-            rest_mb6me3: [],
-            rest_mb6me3_sbref: [],
+            mb1me4: [],
+            mb3me4: [],
+            mb3me4_sbref: [],
+            mb6me4: [],
+            mb6me4_sbref: [],
 
-            rest_mb3me4: [],
-            rest_mb3me4_sbref: [],
-            rest_mb3me3noInt: [],
-            rest_mb3me3noInt_sbref: [],
             }
 
     list_of_ids = [s.series_id for s in seqinfo]
@@ -76,41 +57,29 @@ def infotodict(seqinfo):
 
         # no multi-echo
         if (s.dim4 >= 100) and ('MB1_IP2_ME1' in s.protocol_name):
-            info[rest_mb1me1].append(s.series_id)
+            info[mb1me1].append(s.series_id)
             idx = list_of_ids.index(s.series_id)
-            #info[rest_mb1me1_sbref].append(list_of_ids[idx -1])
         elif (s.dim4 >= 100) and ('MB3_IP2_ME1' in s.protocol_name):
-            info[rest_mb3me1].append(s.series_id)
+            info[mb3me1].append(s.series_id)
             idx = list_of_ids.index(s.series_id)
-            info[rest_mb3me1_sbref].append(list_of_ids[idx -1])
+            info[mb3me1_sbref].append(list_of_ids[idx -1])
         elif (s.dim4 >= 100) and ('MB6_IP2_ME1' in s.protocol_name):
-            info[rest_mb6me1].append(s.series_id)
+            info[mb6me1].append(s.series_id)
             idx = list_of_ids.index(s.series_id)
-            info[rest_mb6me1_sbref].append(list_of_ids[idx -1])
+            info[mb6me1_sbref].append(list_of_ids[idx -1])
 
         # multi-echo standard
-        if (s.dim4 >= 100) and ('MB1_IP2_ME3' in s.protocol_name):
-            info[rest_mb1me3].append(s.series_id)
+        if (s.dim4 >= 100) and ('MB1_IP2_ME4' in s.protocol_name):
+            info[mb1me4].append(s.series_id)
             idx = list_of_ids.index(s.series_id)
-            #info[rest_mb1me3_sbref].append(list_of_ids[idx -1])
-        elif (s.dim4 >= 100) and ('MB3_IP2_ME3' in s.protocol_name):
-            info[rest_mb3me3].append(s.series_id)
+        elif (s.dim4 >= 100) and ('MB3_IP2_ME4' in s.protocol_name):
+            info[mb3me4].append(s.series_id)
             idx = list_of_ids.index(s.series_id)
-            info[rest_mb3me3_sbref].append(list_of_ids[idx -1])
-        elif (s.dim4 >= 100) and ('MB6_IP2_ME3' in s.protocol_name):
-            info[rest_mb6me3].append(s.series_id)
+            info[mb3me4_sbref].append(list_of_ids[idx -1])
+        elif (s.dim4 >= 100) and ('MB6_IP2_ME4' in s.protocol_name):
+            info[mb6me4].append(s.series_id)
             idx = list_of_ids.index(s.series_id)
-            info[rest_mb6me3_sbref].append(list_of_ids[idx -1])
-
-        # extras
-        if (s.dim4 >= 100) and ('MB3_IP2_ME4' in s.protocol_name):
-            info[rest_mb3me4].append(s.series_id)
-            idx = list_of_ids.index(s.series_id)
-            info[rest_mb3me4_sbref].append(list_of_ids[idx -1])
-        elif (s.dim4 >= 100) and ('MB3_IP2_ME3' in s.protocol_name) and ('noInterp' in s.protocol_name):
-            info[rest_mb3me3noInt].append(s.series_id)
-            idx = list_of_ids.index(s.series_id)
-            info[rest_mb3me3noInt_sbref].append(list_of_ids[idx -1])
+            info[mb6me4_sbref].append(list_of_ids[idx -1])
 
 
     return info
