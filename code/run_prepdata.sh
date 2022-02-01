c1b24c13dc54c4b6219a5ee64f2e27483c96fdb5
Jeff <jbdenniso@gmail.com>
2022-02-01 16:09:25 -0500

    cleaning and preparing data

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -4,10 +4,17 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 
-for sub in 1303; do
+sourcedir=/data/sourcedata/rf1-sequence-pilot
+DLscript=${scriptdir}/prepdata/downloadXNAT.py
 
-	script=${scriptdir}/prepdata.sh
-	NCORES=12
+python $DLscript
+
+for sub in $sourcedir;do
+	sub=${sub%_1};
+  	sub=${sub##*-}; 
+
+	script=${scriptdir}/prepdata/prepdata.sh
+	NCORES=8
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
 		sleep 1s
 	done
