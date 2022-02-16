a535c72811b112cf017d178ba2d887af625569e0
orizaff <orizaff@udel.edu>
2022-02-16 14:43:04 -0500

    flipping responses to match instructions

--- stimuli/SharedReward_5button.py
+++ stimuli/SharedReward_5button.py
@@ -281,32 +281,32 @@ def do_run(run, trials):
             #nameStim.draw()
             #win.flip()
 
-            if trial['Feedback'] == '3' and resp_val == 2:
+            if trial['Feedback'] == '3' and resp_val == 7:
                 outcome_txt = int(random.randint(1,4))
                 outcome_moneyTxt= 'h'
                 outcome_color='lime'
                 trials.addData('outcome_val', int(outcome_txt))
-            elif trial['Feedback'] == '3' and resp_val == 7:
+            elif trial['Feedback'] == '3' and resp_val == 2:
                 outcome_txt = int(random.randint(6,9))
                 outcome_moneyTxt= 'h'
                 outcome_color='lime'
-                trials.addData('outcome_val', int(outcome_txt))
-            elif trial['Feedback'] == '2' and resp_val == 2:
-                outcome_txt = int(5)
-                outcome_moneyTxt= 'n'
-                outcome_color='white'
                 trials.addData('outcome_val', int(outcome_txt))
             elif trial['Feedback'] == '2' and resp_val == 7:
                 outcome_txt = int(5)
                 outcome_moneyTxt= 'n'
                 outcome_color='white'
                 trials.addData('outcome_val', int(outcome_txt))
-            elif trial['Feedback'] == '1' and resp_val == 2:
+            elif trial['Feedback'] == '2' and resp_val == 2:
+                outcome_txt = int(5)
+                outcome_moneyTxt= 'n'
+                outcome_color='white'
+                trials.addData('outcome_val', int(outcome_txt))
+            elif trial['Feedback'] == '1' and resp_val == 7:
                 outcome_txt = int(random.randint(6,9))
                 outcome_moneyTxt= 'i'
                 outcome_color='darkred'
                 trials.addData('outcome_val', int(outcome_txt))
-            elif trial['Feedback'] == '1' and resp_val == 7:
+            elif trial['Feedback'] == '1' and resp_val == 2:
                 outcome_txt = int (random.randint(1,4))
                 outcome_moneyTxt= 'i'
                 outcome_color='darkred'
