d16da78293a04fec88c9996a6b98f9694b057c1e
Jeff <jbdenniso@gmail.com>
2022-03-24 14:51:02 -0400

    changes to prepdata for mriqc and make things run more generally

--- code/prepdata.sh
+++ code/prepdata.sh
@@ -12,15 +12,25 @@
 # 3) aside from containers, only absolute path in whole workflow (transparent to folks who aren't allowed to access to raw data)
 sourcedata=/data/sourcedata/rf1-sequence-pilot
 
-
 sub=$1
+
+except_subs=(20022 10007 10003 10006 10008 10010 10014 10015 10026 10028 10030)
+
+for i in "${except_subs[@]}"
+do
+    if [ "$i" -eq "$sub" ] ; then
+        echo "Exception ${sub}"
+	exit 1
+    fi
+done
 
 
 # ensure paths are correct irrespective from where user runs the script
 codedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dsroot="$(dirname "$codedir")"
 
+dsroot=/data/projects/rf1-sequence-pilot
 
+echo ${dsroot}
 
 # make bids folder if it doesn't exist
 if [ ! -d $dsroot/bids ]; then
@@ -28,24 +38,29 @@ if [ ! -d $dsroot/bids ]; then
 fi
 
 # overwrite existing
-rm -rf $dsroot/bids/sub-${sub}
+#rm -rf $dsroot/bids/sub-${sub}
 
 
 # PART 1: running heudiconv and fixing fieldmaps
 #/data/sourcedata/rf1-sequence-pilot/MB_ME_test_sub-0001/dicoms/9-CMRR_MB3_IP2_ME1_TR1850/resources/DICOM/files
+if [ ! -d $dsroot/bids/sub-${sub} ]; then
 
-singularity run --cleanenv \
--B $dsroot:/out \
--B $sourcedata:/sourcedata \
-/data/tools/heudiconv-0.9.0.simg \
--d /sourcedata/Smith-SRA-{subject}/*/scans/*/*/DICOM/files/*.dcm \
--s $sub \
--f /out/code/heuristics.py \
--c dcm2niix -b --minmeta -o /out/bids --overwrite
+ echo "making bids for sub-${sub}"
+
+	singularity run --cleanenv \
+	-B $dsroot:/out \
+	-B $sourcedata:/sourcedata \
+	/data/tools/heudiconv-0.9.0.simg \
+	-d /sourcedata/Smith-SRA-{subject}/*/scans/*/*/DICOM/files/*.dcm \
+	-s $sub \
+	-f /out/code/heuristics.py \
+	-c dcm2niix -b --minmeta -o /out/bids --overwrite
+
+fi
 
 # run Jeff's code to fix field map, but first correct permissions
-#chmod -R uga+rw $dsroot/bids/sub-$sub
-#python $codedir/addIntendedFor.py
+chmod -R uga+rw $dsroot/bids/sub-$sub
+python $codedir/addIntendedFor.py
 
 
 #
@@ -65,27 +80,32 @@ python $codedir/shiftdates.py $dsroot/bids/sub-${sub}/sub-${sub}_scans.tsv
 #
 ## make derivatives folder if it doesn't exist.
 ## let's keep this out of bids for now
-#if [ ! -d $dsroot/derivatives/mriqc ]; then
-#	mkdir -p $dsroot/derivatives/mriqc
-#fi
-#
-#
-## make scratch
-#scratch=/data/scratch/`whoami`
-#if [ ! -d $scratch ]; then
-#	mkdir -p $scratch
-#fi
-#
-## no space left on device error for v0.15.2 and higher
-## https://neurostars.org/t/mriqc-no-space-left-on-device-error/16187/1
-## https://github.com/poldracklab/mriqc/issues/850
-#TEMPLATEFLOW_DIR=/data/tools/templateflow
-#export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
-#singularity run --cleanenv \
-#-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
-#-B $dsroot/bids:/data \
-#-B $dsroot/derivatives/mriqc:/out \
-#-B $scratch:/scratch \
-#/data/tools/mriqc-0.16.1.simg \
-#/data /out \
-#participant --participant_label $sub -w /scratch
+if [ ! -d $dsroot/derivatives/mriqc ]; then
+	mkdir -p $dsroot/derivatives/mriqc
+fi
+
+
+# make scratch
+scratch=/data/scratch/`whoami`
+if [ ! -d $scratch ]; then
+	mkdir -p $scratch
+fi
+
+# no space left on device error for v0.15.2 and higher
+# https://neurostars.org/t/mriqc-no-space-left-on-device-error/16187/1
+# https://github.com/poldracklab/mriqc/issues/850
+TEMPLATEFLOW_DIR=/data/tools/templateflow
+export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
+if [ ! -d $dsroot/derivatives/mriqc/sub-${sub} ]; then
+ echo "running mriqc for sub-${sub}"
+	
+	singularity run --cleanenv \
+	-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+	-B $dsroot/bids:/data \
+	-B $dsroot/derivatives/mriqc:/out \
+	-B $scratch:/scratch \
+	/data/tools/mriqc-0.16.1.simg \
+
+fi
+/data /out \
+participant --participant_label $sub -w /scratch
