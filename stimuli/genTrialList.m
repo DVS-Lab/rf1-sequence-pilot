e653a41e4f9ec2033d8949fa3c112fbc2b8a70bb
danielsazhin <54955287+danielsazhin@users.noreply.github.com>
2022-03-28 15:46:39 -0400

    sra sunj 10041 fmri

--- stimuli/genTrialList.m
+++ stimuli/genTrialList.m
@@ -27,7 +27,7 @@ evt6	stranger_win        11
 maindir = pwd;
 outfiles = fullfile(maindir,'event-related','params');
 mkdir(outfiles);
-subs = [10001:10040];
+subs = [10044:10200];
 
 for s = subs
 
