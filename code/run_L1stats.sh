d7646e7c89bd99e339ac0cd0dbb408f7cfadbb7e
orizaff <orizaff@udel.edu>
2022-07-08 11:44:18 -0400

    added check for mean button press to determine if empty EVs

--- code/run_L1stats.sh
+++ code/run_L1stats.sh
@@ -7,7 +7,7 @@ basedir="$(dirname "$scriptdir")"
 task=sharedreward # edit if necessary
 
 for ppi in 0 VS; do # putting 0 first will indicate "activation"
-	for sub in 10024; do # 10017 10024 10035 10041 10043 10054 10059 10069 10074 10078 10080 10085 10094 10108 10125 10130 10136 10137 10142 10150 10154 10186 10188 10221
+	for sub in 10017 10024 10035 10043 10054 10059 10074 10080 10108 10125 10136 10137 10142 10150 10154 10186 10188 10221; do
 		for mb in 1 3 6; do
 			for me in 1 4; do
 				# Manages the number of jobs and cores
