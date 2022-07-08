3a81e6de9822803b413a8a3c13f49bd0a2b1c114
DVSneuro <david.v.smith@temple.edu>
2022-07-07 23:24:20 -0400

    remove unused lines and fix indentation

--- code/run_L1stats.sh
+++ code/run_L1stats.sh
@@ -6,27 +6,19 @@ basedir="$(dirname "$scriptdir")"
 
 task=sharedreward # edit if necessary
 
-for ppi in VS; do # putting 0 first will indicate "activation"
-
-	for sub in 10024; do # 10017 10024 10035 10041 10043 10054 10059 10069 10074 10078 10080 10085 10094 10108 10125 10130 10136 10137 10142 10150 10154 10186 10188 10221 `ls -d ${basedir}/derivatives/fmriprep/sub-*/`; do
-
-          sub=${sub#*sub-}
-          sub=${sub%/}  
-
-	  for mb in 1 3 6; do
-		for me in 1 4; do
-
-	  	# Manages the number of jobs and cores
-	  	SCRIPTNAME=${basedir}/code/L1stats.sh
-	  	NCORES=15
-	  	while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
-	    		sleep 5s
-	  	done
-	  	bash $SCRIPTNAME $sub $mb $me $ppi &
-                echo $SCRIPTNAME $sub $mb $me $ppi &
-			sleep 1s
-
-	    done
-	  done
+for ppi in 0 VS; do # putting 0 first will indicate "activation"
+	for sub in 10024; do # 10017 10024 10035 10041 10043 10054 10059 10069 10074 10078 10080 10085 10094 10108 10125 10130 10136 10137 10142 10150 10154 10186 10188 10221
+		for mb in 1 3 6; do
+			for me in 1 4; do
+				# Manages the number of jobs and cores
+				SCRIPTNAME=${basedir}/code/L1stats.sh
+				NCORES=15
+				while [ $(ps -ef | grep -v grep | grep $SCRIPTNAME | wc -l) -ge $NCORES ]; do
+					sleep 5s
+				done
+				bash $SCRIPTNAME $sub $mb $me $ppi &
+				sleep 1s
+			done
+		done
 	done
 done
